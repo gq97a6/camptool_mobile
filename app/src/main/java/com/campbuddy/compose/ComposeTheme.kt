5f9ba1ab413dcbfe161be27c93567871efd3beef
gq97a6 <sz.kg@protonmail.com>
2023-12-17 23:46:09 +0100

    Navigation drawer added. Status bar color set.

--- app/src/main/java/com/campbuddy/compose/ComposeTheme.kt
+++ app/src/main/java/com/campbuddy/compose/ComposeTheme.kt
@@ -1,5 +1,6 @@
 package com.campbuddy.compose
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -8,12 +9,16 @@ import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 
 
 val seed = Color(0xFFFF9900)
@@ -163,14 +168,23 @@ fun Theme(isDark: Boolean = isSystemInDarkTheme(), content: @Composable () -> Un
 
     val colorScheme = if (isDark) darkColorScheme else lightColorScheme
 
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !isDark
+        }
+    }
+
     MaterialTheme(
         content = content,
         colorScheme = colorScheme,
         typography = Typography,
-        shapes = Shapes(
-            small = RoundedCornerShape(4.dp),
-            medium = RoundedCornerShape(4.dp),
-            large = RoundedCornerShape(4.dp)
-        )
+        //shapes = Shapes(
+        //    small = RoundedCornerShape(10.dp),
+        //    medium = RoundedCornerShape(10.dp),
+        //    large = RoundedCornerShape(10.dp)
+        //)
     )
 }
