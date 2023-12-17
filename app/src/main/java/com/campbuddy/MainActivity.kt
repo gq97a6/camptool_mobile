1b718df0f9c7653d0eef9e79f89008890c2cf99d
gq97a6 <szymon.kogut@protonmail.com>
2023-12-17 23:46:09 +0100

    Navigation drawer added. Status bar color set.

--- app/src/main/java/com/campbuddy/MainActivity.kt
+++ app/src/main/java/com/campbuddy/MainActivity.kt
@@ -6,30 +6,53 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Assignment
+import androidx.compose.material.icons.automirrored.outlined.ListAlt
+import androidx.compose.material.icons.automirrored.outlined.Login
 import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material.icons.outlined.ContactPage
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.campbuddy.compose.Theme
 import com.campbuddy.`object`.Mockup
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,22 +67,122 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun NavigationDrawer(navController: NavHostController, content: @Composable () -> Unit) {
+    val locations = listOf(
+        "home",
+        "login",
+        "settings",
+        "day",
+        "planner",
+        "card",
+        "list",
+    )
+
+    val icons = listOf(
+        Icons.Outlined.Home,
+        Icons.AutoMirrored.Outlined.Login,
+        Icons.Outlined.Settings,
+        Icons.AutoMirrored.Outlined.ListAlt,
+        Icons.AutoMirrored.Outlined.Assignment,
+        Icons.Outlined.AccountBox,
+        Icons.Outlined.ContactPage
+    )
+
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        content = content,
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(M.width(100.dp)) {
+                repeat(locations.size) {
+                    IconButton(modifier = M
+                        .fillMaxWidth()
+                        .aspectRatio(1f), onClick = {
+                        scope.launch { drawerState.close() }
+                        navController.navigate(locations[it])
+                        navController.clearBackStack(locations[it])
+                    }) {
+                        Icon(
+                            modifier = M.fillMaxSize(.5f),
+                            imageVector = icons[it],
+                            contentDescription = "Favorite",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+                //NavigationDrawerItem(
+                //    label = { },
+                //    selected = false,
+                //    icon = {
+                //        Icon(
+                //            imageVector = Icons.Outlined.AccountBox,
+                //            contentDescription = "Favorite",
+                //            tint = MaterialTheme.colorScheme.primary
+                //        )
+                //    },
+                //    onClick = {}
+                //)
+            }
+        }
+    )
+}
+
+@Composable
 fun AppNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "home") {
-        composable("home") { HomeScreen(navController) }
-        composable("login") { LoginScreen(navController) }
-        composable("settings") { Settings(navController) }
-        composable("day") { DayPlan(navController) }
-        composable("planner") { Planner(navController) }
-        composable("card") { Card(navController) }
-        composable("list") { CheckList(navController) }
+    NavigationDrawer(navController) {
+        NavHost(navController = navController, startDestination = "home") {
+            composable("home") { HomeScreen(navController) }
+            composable("login") { LoginScreen(navController) }
+            composable("settings") { Settings(navController) }
+            composable("day") { DayPlan(navController) }
+            composable("planner") { Planner(navController) }
+            composable("card") { Card(navController) }
+            composable("list") { CheckList(navController) }
+        }
     }
 }
 
 @Composable
-fun HomeScreen(navController: NavController) = Column {
+fun HomeScreen(navController: NavController) {
+    Text(text = "home", fontSize = 50.sp, color = MaterialTheme.colorScheme.onBackground)
+}
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    Text(text = "login", fontSize = 50.sp, color = MaterialTheme.colorScheme.onBackground)
+}
+
+@Composable
+fun Settings(navController: NavController) {
+    Text(text = "settings", fontSize = 50.sp, color = MaterialTheme.colorScheme.onBackground)
+}
+
+@Composable
+fun DayPlan(navController: NavController) {
+    Text(text = "day_plan", fontSize = 50.sp, color = MaterialTheme.colorScheme.onBackground)
+}
+
+@Composable
+fun Planner(navController: NavController) {
+    Text(text = "planner", fontSize = 50.sp, color = MaterialTheme.colorScheme.onBackground)
+}
+
+@Composable
+fun Card(navController: NavController) {
+    Text(text = "card", fontSize = 50.sp, color = MaterialTheme.colorScheme.onBackground)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CheckList(navController: NavController) = Column {
+
+    val scope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState()
+    var sheetStatus by remember { mutableStateOf(true) }
 
     // Remember a list that tracks the checked states of each checkbox
     val checkboxStates = remember { mutableStateListOf(*Array(100) { false }) }
@@ -67,12 +190,15 @@ fun HomeScreen(navController: NavController) = Column {
     LazyColumn(M.padding(horizontal = 10.dp)) {
         itemsIndexed(items = Mockup.names) { i, name ->
             Row(
-                M
-                    .fillMaxWidth(),
+                M.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(text = name)
+                Text(
+                    text = name,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = M.fillMaxWidth(.7f)
+                )
                 IconButton(onClick = { }) {
                     Icon(
                         imageVector = Icons.Outlined.AccountBox,
@@ -90,56 +216,21 @@ fun HomeScreen(navController: NavController) = Column {
         }
     }
 
-    //val sheetState = rememberSheetState()
-    //val scope = rememberCoroutineScope()
-    //var showBottomSheet by remember { mutableStateOf(false) }
-
-    //Button(onClick = { showBottomSheet = true }) {
-    //    Text(text = "SHOW")
-    //}
-
-    //if (showBottomSheet) ModalBottomSheet(
-    //    onDismissRequest = { showBottomSheet = false },
-    //    sheetState = sheetState
-    //) {
-    //    Button(onClick = {
-    //        scope.launch { sheetState.hide() }.invokeOnCompletion {
-    //            if (!sheetState.isVisible) {
-    //                showBottomSheet = false
-    //            }
-    //        }
-    //    }) {
-    //        Text("Hide bottom sheet")
-    //    }
-    //}
-}
-
-@Composable
-fun LoginScreen(navController: NavController) {
-    Text(text = "login", fontSize = 50.sp)
-}
-
-@Composable
-fun Settings(navController: NavController) {
-    Text(text = "settings", fontSize = 50.sp)
-}
-
-@Composable
-fun DayPlan(navController: NavController) {
-    Text(text = "day_plan", fontSize = 50.sp)
-}
-
-@Composable
-fun Planner(navController: NavController) {
-    Text(text = "planner", fontSize = 50.sp)
-}
-
-@Composable
-fun Card(navController: NavController) {
-    Text(text = "card", fontSize = 50.sp)
-}
-
-@Composable
-fun CheckList(navController: NavController) {
-    Text(text = "list", fontSize = 50.sp)
+    if (sheetStatus) {
+        ModalBottomSheet(
+            onDismissRequest = { sheetStatus = false },
+            sheetState = sheetState,
+            modifier = M.height(400.dp)
+        ) {
+            //Button(onClick = {
+            //    scope.launch { sheetState.hide() }.invokeOnCompletion {
+            //        if (!sheetState.isVisible) {
+            //            sheetStatus = false
+            //        }
+            //    }
+            //}) {
+            //    Text("Hide bottom sheet")
+            //}
+        }
+    }
 }
