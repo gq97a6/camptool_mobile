18963c3f47debe9936c45b6a5c08ecf0c9639f78
gq97a6 <szymon.kogut@protonmail.com>
2023-12-27 04:34:14 +0100

    List drawer added.

--- app/src/main/java/com/campbuddy/MainActivity.kt
+++ app/src/main/java/com/campbuddy/MainActivity.kt
@@ -3,10 +3,20 @@ package com.campbuddy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.foundation.gestures.DraggableAnchors
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.anchoredDraggable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,14 +32,12 @@ import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.material.icons.outlined.ContactPage
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
@@ -43,6 +51,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -91,42 +101,26 @@ fun NavigationDrawer(navController: NavHostController, content: @Composable () -
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
-    ModalNavigationDrawer(
-        content = content,
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet(M.width(100.dp)) {
-                repeat(locations.size) {
-                    IconButton(modifier = M
-                        .fillMaxWidth()
-                        .aspectRatio(1f), onClick = {
-                        scope.launch { drawerState.close() }
-                        navController.navigate(locations[it])
-                        navController.clearBackStack(locations[it])
-                    }) {
-                        Icon(
-                            modifier = M.fillMaxSize(.5f),
-                            imageVector = icons[it],
-                            contentDescription = "Favorite",
-                            tint = MaterialTheme.colorScheme.primary
-                        )
-                    }
+    ModalNavigationDrawer(content = content, drawerState = drawerState, drawerContent = {
+        ModalDrawerSheet(M.width(100.dp)) {
+            repeat(locations.size) {
+                IconButton(modifier = M
+                    .fillMaxWidth()
+                    .aspectRatio(1f), onClick = {
+                    scope.launch { drawerState.close() }
+                    navController.navigate(locations[it])
+                    navController.clearBackStack(locations[it])
+                }) {
+                    Icon(
+                        modifier = M.fillMaxSize(.5f),
+                        imageVector = icons[it],
+                        contentDescription = "Favorite",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
                 }
-                //NavigationDrawerItem(
-                //    label = { },
-                //    selected = false,
-                //    icon = {
-                //        Icon(
-                //            imageVector = Icons.Outlined.AccountBox,
-                //            contentDescription = "Favorite",
-                //            tint = MaterialTheme.colorScheme.primary
-                //        )
-                //    },
-                //    onClick = {}
-                //)
             }
         }
-    )
+    })
 }
 
 @Composable
@@ -134,7 +128,7 @@ fun AppNavigation() {
     val navController = rememberNavController()
 
     NavigationDrawer(navController) {
-        NavHost(navController = navController, startDestination = "home") {
+        NavHost(navController = navController, startDestination = "list") {
             composable("home") { HomeScreen(navController) }
             composable("login") { LoginScreen(navController) }
             composable("settings") { Settings(navController) }
@@ -183,54 +177,85 @@ fun CheckList(navController: NavController) = Column {
     val scope = rememberCoroutineScope()
     val sheetState = rememberModalBottomSheetState()
     var sheetStatus by remember { mutableStateOf(true) }
-
-    // Remember a list that tracks the checked states of each checkbox
     val checkboxStates = remember { mutableStateListOf(*Array(100) { false }) }
 
-    LazyColumn(M.padding(horizontal = 10.dp)) {
-        itemsIndexed(items = Mockup.names) { i, name ->
-            Row(
-                M.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = name,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    modifier = M.fillMaxWidth(.7f)
-                )
-                IconButton(onClick = { }) {
-                    Icon(
-                        imageVector = Icons.Outlined.AccountBox,
-                        contentDescription = "Favorite",
-                        tint = MaterialTheme.colorScheme.primary
+
+    HorizontalDraggableSample {
+        LazyColumn(M.padding(horizontal = 10.dp).fillMaxHeight().padding(bottom = 100.dp)) {
+            itemsIndexed(items = Mockup.names) { i, name ->
+                Row(
+                    M.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = name,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = M.fillMaxWidth(.7f)
+                    )
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Outlined.AccountBox,
+                            contentDescription = "Favorite",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                    Checkbox(
+                        checked = checkboxStates[i],
+                        onCheckedChange = { checked ->
+                            checkboxStates[i] = checked
+                        }
                     )
                 }
-                Checkbox(
-                    checked = checkboxStates[i],
-                    onCheckedChange = { checked ->
-                        checkboxStates[i] = checked
-                    }
-                )
             }
         }
     }
+}
 
-    if (sheetStatus) {
-        ModalBottomSheet(
-            onDismissRequest = { sheetStatus = false },
-            sheetState = sheetState,
-            modifier = M.height(400.dp)
-        ) {
-            //Button(onClick = {
-            //    scope.launch { sheetState.hide() }.invokeOnCompletion {
-            //        if (!sheetState.isVisible) {
-            //            sheetStatus = false
-            //        }
-            //    }
-            //}) {
-            //    Text("Hide bottom sheet")
-            //}
-        }
+enum class DragAnchors(val fraction: Float) {
+    Start(0f),
+    //Half(.5f),
+    End(.9f),
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HorizontalDraggableSample(content: @Composable () -> Unit) {
+    val size = 100.dp.toPx()
+
+    val state = remember {
+        AnchoredDraggableState(
+            initialValue = DragAnchors.Start,
+            positionalThreshold = { it * 0.5f },
+            velocityThreshold = { 0f },
+            animationSpec = tween(),
+        )
+    }
+
+    Box(
+        modifier = M
+            .onSizeChanged { layoutSize ->
+                val end = layoutSize.height - size
+                state.updateAnchors(DraggableAnchors {
+                    DragAnchors
+                        .values()
+                        .forEach { anchor ->
+                            anchor at end * anchor.fraction
+                        }
+                })
+            }
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        content()
+
+        Box(
+            modifier = Modifier
+                .height(100.dp + state.offset.toDp())
+                .fillMaxWidth()
+                //.offset { IntOffset(x = 0, y = state.offset.roundToInt()) }
+                .background(MaterialTheme.colorScheme.surfaceVariant, MaterialTheme.shapes.medium)
+                .anchoredDraggable(state, Orientation.Vertical, reverseDirection = true)
+        )
     }
 }
