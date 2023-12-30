01b603c07bb5312300d62cc507dce79a8417e862
gq97a6 <szymon.kogut@protonmail.com>
2023-12-30 04:46:20 +0100

    Basic checklist implemented.

--- app/src/main/java/com/campbuddy/MainActivity.kt
+++ app/src/main/java/com/campbuddy/MainActivity.kt
@@ -1,29 +1,15 @@
 package com.campbuddy
 
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.AnchoredDraggableState
-import androidx.compose.foundation.gestures.DraggableAnchors
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.anchoredDraggable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Assignment
 import androidx.compose.material.icons.automirrored.outlined.ListAlt
@@ -32,9 +18,8 @@ import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.material.icons.outlined.ContactPage
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -42,17 +27,9 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -61,7 +38,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.campbuddy.compose.Theme
-import com.campbuddy.`object`.Mockup
+import com.campbuddy.layout.ListScreen
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -135,7 +112,7 @@ fun AppNavigation() {
             composable("day") { DayPlan(navController) }
             composable("planner") { Planner(navController) }
             composable("card") { Card(navController) }
-            composable("list") { CheckList(navController) }
+            composable("list") { ListScreen(navController) }
         }
     }
 }
@@ -168,94 +145,4 @@ fun Planner(navController: NavController) {
 @Composable
 fun Card(navController: NavController) {
     Text(text = "card", fontSize = 50.sp, color = MaterialTheme.colorScheme.onBackground)
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CheckList(navController: NavController) = Column {
-
-    val scope = rememberCoroutineScope()
-    val sheetState = rememberModalBottomSheetState()
-    var sheetStatus by remember { mutableStateOf(true) }
-    val checkboxStates = remember { mutableStateListOf(*Array(100) { false }) }
-
-
-    HorizontalDraggableSample {
-        LazyColumn(M.padding(horizontal = 10.dp).fillMaxHeight().padding(bottom = 100.dp)) {
-            itemsIndexed(items = Mockup.names) { i, name ->
-                Row(
-                    M.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text = name,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = M.fillMaxWidth(.7f)
-                    )
-                    IconButton(onClick = { }) {
-                        Icon(
-                            imageVector = Icons.Outlined.AccountBox,
-                            contentDescription = "Favorite",
-                            tint = MaterialTheme.colorScheme.primary
-                        )
-                    }
-                    Checkbox(
-                        checked = checkboxStates[i],
-                        onCheckedChange = { checked ->
-                            checkboxStates[i] = checked
-                        }
-                    )
-                }
-            }
-        }
-    }
-}
-
-enum class DragAnchors(val fraction: Float) {
-    Start(0f),
-    //Half(.5f),
-    End(.9f),
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun HorizontalDraggableSample(content: @Composable () -> Unit) {
-    val size = 100.dp.toPx()
-
-    val state = remember {
-        AnchoredDraggableState(
-            initialValue = DragAnchors.Start,
-            positionalThreshold = { it * 0.5f },
-            velocityThreshold = { 0f },
-            animationSpec = tween(),
-        )
-    }
-
-    Box(
-        modifier = M
-            .onSizeChanged { layoutSize ->
-                val end = layoutSize.height - size
-                state.updateAnchors(DraggableAnchors {
-                    DragAnchors
-                        .values()
-                        .forEach { anchor ->
-                            anchor at end * anchor.fraction
-                        }
-                })
-            }
-            .fillMaxSize(),
-        contentAlignment = Alignment.BottomCenter
-    ) {
-        content()
-
-        Box(
-            modifier = Modifier
-                .height(100.dp + state.offset.toDp())
-                .fillMaxWidth()
-                //.offset { IntOffset(x = 0, y = state.offset.roundToInt()) }
-                .background(MaterialTheme.colorScheme.surfaceVariant, MaterialTheme.shapes.medium)
-                .anchoredDraggable(state, Orientation.Vertical, reverseDirection = true)
-        )
-    }
 }
