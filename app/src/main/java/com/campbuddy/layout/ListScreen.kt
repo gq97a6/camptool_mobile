a6c4f7464305db4acb40cc56592530a7b967aaf1
gq97a6 <szymon.kogut@protonmail.com>
2024-01-19 04:16:43 +0100

    ListScreen layout done.

--- app/src/main/java/com/campbuddy/layout/ListScreen.kt
+++ app/src/main/java/com/campbuddy/layout/ListScreen.kt
@@ -2,72 +2,56 @@
 
 package com.campbuddy.layout
 
-import android.graphics.Color
-import android.graphics.RenderEffect
-import android.graphics.Shader
-import android.os.Build
-import android.widget.FrameLayout
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.AnchoredDraggableState
-import androidx.compose.foundation.gestures.DraggableAnchors
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.anchoredDraggable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.BlurEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import com.campbuddy.compose.BottomDrawer
 import com.campbuddy.compose.Theme
 import com.campbuddy.`object`.Mockup
 import com.campbuddy.performClick
-import com.campbuddy.toDp
-import com.campbuddy.toPx
-
-enum class DragAnchors(val fraction: Float) {
-    Start(0f),
-
-    //Mid(.3f),
-    End(.9f)
-}
 
 @Composable
 @Preview
@@ -83,7 +67,7 @@ fun PreviewDark() {
 }
 
 @Composable
-@Preview
+//@Preview
 fun PreviewLight() {
     Theme(false) {
         Box(
@@ -95,11 +79,22 @@ fun PreviewLight() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ListScreen(navController: NavController?) {
 
     //val scope = rememberCoroutineScope()
     var states = remember { mutableStateListOf(*Array(100) { true }) }
+    var filter by remember { mutableStateOf(2) }
+
+    val drawerState = remember {
+        AnchoredDraggableState(
+            initialValue = 0,
+            positionalThreshold = { it * 0.5f },
+            velocityThreshold = { 0f },
+            animationSpec = tween(),
+        )
+    }
 
     LazyColumn(
         Modifier
@@ -107,124 +102,107 @@ fun ListScreen(navController: NavController?) {
             .fillMaxHeight()
     ) {
         itemsIndexed(items = Mockup.names) { i, name ->
-            Row(
+            when (filter) {
+                0 -> if (!states[i]) return@itemsIndexed
+                1 -> if (states[i]) return@itemsIndexed
+            }
+
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(100.dp)
-                    .padding(top = 15.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                //horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                        .border(
-                            2.dp,
-                            MaterialTheme.colorScheme.primary,
-                            MaterialTheme.shapes.small
-                        )
-                        .padding(start = 12.dp),
-                    contentAlignment = Alignment.CenterStart
-                ) {
-                    Text(
-                        text = name.replace(" ", "\n"),
-                        color = MaterialTheme.colorScheme.primary,
-                        style = TextStyle(
-                            fontSize = 44.sp,
-                            shadow = Shadow(
-                                color = MaterialTheme.colorScheme.scrim, //TODO: CHECK COLOR
-                                offset = Offset(1.0f, 2.0f),
-                                blurRadius = 1f
-                            )
-                        ),
-                        fontSize = 24.sp,
-                        softWrap = true,
-                        lineHeight = 34.sp,
-                        modifier = Modifier.fillMaxWidth()
+                    .padding(top = 15.dp)
+                    .border(
+                        2.dp,
+                        MaterialTheme.colorScheme.primary,
+                        MaterialTheme.shapes.small
                     )
-                }
+                    .clickable {
+                        states[i] = !states[i]
+                        navController?.context?.apply { performClick(this) }
+                    }
+            ) {
+                Text(
+                    text = name.replace(" ", "\n"),
+                    color = MaterialTheme.colorScheme.primary,
+                    style = TextStyle(
+                        fontSize = 22.sp,
+                        shadow = Shadow(
+                            color = MaterialTheme.colorScheme.scrim, //TODO: CHECK COLOR
+                            offset = Offset(1.0f, 2.0f),
+                            blurRadius = 1f
+                        )
+                    ),
+                    softWrap = true,
+                    lineHeight = 34.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.CenterStart)
+                        .padding(start = 30.dp)
+                )
 
                 Box(
                     Modifier
-                        .padding(start = 15.dp)
+                        .align(Alignment.CenterStart)
                         .fillMaxHeight()
-                        .aspectRatio(1f)
+                        .width(20.dp)
                         .border(
                             2.dp,
                             MaterialTheme.colorScheme.primary,
-                            MaterialTheme.shapes.small
+                            MaterialTheme.shapes.large.copy(
+                                topEnd = CornerSize(0),
+                                bottomEnd = CornerSize(0)
+                            )
                         )
-                        .clickable {
-                            states[i] = !states[i]
-                            navController?.context?.apply { performClick(this) }
-                        }
-                ) {
-                    if (states[i]) {
-                        Icon(
-                            imageVector = Icons.Default.Check,
-                            contentDescription = "checkbox",
-                            tint = MaterialTheme.colorScheme.primary,
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(10.dp),
+                        .then(
+                            if (states[i]) Modifier.background(
+                                MaterialTheme.colorScheme.primary,
+                                MaterialTheme.shapes.large.copy(
+                                    topEnd = CornerSize(0),
+                                    bottomEnd = CornerSize(0)
+                                )
+                            ) else Modifier
                         )
-                    }
-                }
+                )
             }
         }
 
         item { Spacer(modifier = Modifier.height(115.dp)) }
     }
 
-    BottomDrawer(100.dp, 2.dp) {
-        Column(Modifier.fillMaxSize().padding(15.dp)) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp)
-            ) {
-                ListScreenDrawerContentSmall(states.size, states.count { it })
-            }
+    //DRAWER ---------------------------------------------------------------------------------------
 
-            Box(modifier = Modifier.fillMaxSize()) {
-                //ListScreenDrawerContentBig(states)
-            }
-        }
-    }
-}
-
-@Composable
-fun ListScreenDrawerContentSmall(all: Int, checked: Int) {
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(
-            text = "$checked / $all",
-            color = MaterialTheme.colorScheme.primary,
-            style = TextStyle(
-                fontSize = 44.sp,
-                shadow = Shadow(
-                    color = MaterialTheme.colorScheme.scrim, //TODO: CHECK COLOR
-                    offset = Offset(1.0f, 2.0f),
-                    blurRadius = 1f
+    BottomDrawer(listOf(220.dp, 0.dp), {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .height(320.dp)
+                .offset(y = 2.dp)
+                .border(
+                    2.dp,
+                    MaterialTheme.colorScheme.primary,
+                    MaterialTheme.shapes.large.copy(
+                        bottomEnd = CornerSize(0),
+                        bottomStart = CornerSize(0)
+                    )
                 )
-            ),
-            textAlign = TextAlign.Start,
-            fontSize = 35.sp,
-            softWrap = true,
-            lineHeight = 34.sp,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-    }
-}
-
-@Composable
-fun ListScreenDrawerContentBig(states: SnapshotStateList<Boolean>) {
-    Column(Modifier.fillMaxSize()) {
-        Mockup.names.forEachIndexed { i, name ->
-            if (states[i]) {
+                .alpha(.98f)
+                .background(
+                    MaterialTheme.colorScheme.background,
+                    MaterialTheme.shapes.large.copy(
+                        bottomEnd = CornerSize(0),
+                        bottomStart = CornerSize(0)
+                    )
+                )
+        ) {
+            Box(
+                Modifier
+                    .height(100.dp)
+                    .padding(start = 15.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
                 Text(
-                    text = "$i. $name",
+                    text = "100 / 100",
                     color = MaterialTheme.colorScheme.primary,
                     style = TextStyle(
                         fontSize = 44.sp,
@@ -235,12 +213,44 @@ fun ListScreenDrawerContentBig(states: SnapshotStateList<Boolean>) {
                         )
                     ),
                     textAlign = TextAlign.Start,
-                    fontSize = 25.sp,
+                    fontSize = 35.sp,
                     softWrap = true,
-                    lineHeight = 34.sp,
-                    modifier = Modifier.fillMaxWidth()
+                    lineHeight = 34.sp
                 )
             }
+
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 15.dp)) {
+                listOf(
+                    "Pokaż jedynie obecnych",
+                    "Pokaż jedynie nieobecnych",
+                    "Pokaż wszystkich"
+                ).forEachIndexed { index, s ->
+                    FilterChip(
+                        selected = filter == index,
+                        onClick = { filter = index },
+                        label = { Text(s) },
+                        leadingIcon = {
+                            if (filter == index) Icon(
+                                imageVector = Icons.Filled.Done,
+                                contentDescription = "",
+                                modifier = Modifier.size(FilterChipDefaults.IconSize)
+                            )
+                        },
+                    )
+                }
+
+                OutlinedButton(
+                    onClick = {
+                        repeat(states.size) { states[it] = false }
+                        filter = 2
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Odznacz wszystkich")
+                }
+            }
         }
-    }
+    })
 }
